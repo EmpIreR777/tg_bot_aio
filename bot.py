a447e2dd18617bdbba788c57e82b2e5f06f46c43
Kirill Safronov <Kirill.tanatos1993@mail.ru>
2024-11-29 22:25:46 +0300

    fix

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
+from random import randint
 import re
 from datetime import datetime
+from typing import Optional
 import asyncio
 import logging
+from contextlib import suppress
 from aiogram import F, html
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command, CommandObject, CommandStart
@@ -10,11 +13,14 @@ from aiogram.enums import ParseMode
 from aiogram.utils.markdown import hide_link
 from aiogram.utils.formatting import Text, Bold
 from aiogram.utils.media_group import MediaGroupBuilder
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+from aiogram.exceptions import TelegramBadRequest
+from aiogram.filters.callback_data import CallbackData
+import httpx
+from magic_filter import F as FFF
 
 from config_reader import config
 
-import httpx
 
 logging.basicConfig(level=logging.INFO)
 
@@ -22,6 +28,152 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config.bot_token.get_secret_value())
 dp = Dispatcher()
 dp["started_at"] = datetime.now().strftime("%Y-%m-%d %H:%M")
+
+
+user_data = {}
+
+
+class NumbersCallbackFactory(CallbackData, prefix='fabnum'):
+    action: str
+    value: Optional[int] = None
+
+
+def get_keyboard_fab():
+    builder = InlineKeyboardBuilder()
+    builder.button(
+        text="-2", callback_data=NumbersCallbackFactory(action="change", value=-2)
+    )
+    builder.button(
+        text="-1", callback_data=NumbersCallbackFactory(action="change", value=-1)
+    )
+    builder.button(
+        text="+1", callback_data=NumbersCallbackFactory(action="change", value=1)
+    )
+    builder.button(
+        text="+2", callback_data=NumbersCallbackFactory(action="change", value=2)
+    )
+    builder.button(
+        text="Подтвердить", callback_data=NumbersCallbackFactory(action="finish")
+    )
+    # Выравниваем кнопки по 4 в ряд, чтобы получилось 4 + 1
+    builder.adjust(4)
+    return builder.as_markup()
+
+
+async def update_num_text_fab(message: types.Message, new_value: int):
+    with suppress(TelegramBadRequest):
+        await message.edit_text(f"Укажите число: {new_value}", reply_markup=get_keyboard_fab())
+
+
+@dp.message(Command("numbers_fab"))
+async def cmd_numbers_fab(message: types.Message):
+    user_data[message.from_user.id] = 0
+    await message.answer("Укажите число: 0", reply_markup=get_keyboard_fab())
+
+
+@dp.callback_query(NumbersCallbackFactory.filter(F.action == "change"))
+async def callbacks_num_change_fab(
+        callback: types.CallbackQuery, 
+        callback_data: NumbersCallbackFactory
+):
+    # Текущее значение
+    user_value = user_data.get(callback.from_user.id, 0)
+
+    user_data[callback.from_user.id] = user_value + callback_data.value
+    await update_num_text_fab(callback.message, user_value + callback_data.value)
+    await callback.answer()
+
+
+@dp.callback_query(NumbersCallbackFactory.filter(F.action == "finish"))
+async def callbacks_num_finish_fab(callback: types.CallbackQuery):
+    # Текущее значение
+    user_value = user_data.get(callback.from_user.id, 0)
+
+    await callback.message.edit_text(f"Итого: {user_value}")
+    await callback.answer()
+
+
+def get_keyboard():
+    buttons = [
+        [
+            types.InlineKeyboardButton(text="-1", callback_data="num_decr"),
+            types.InlineKeyboardButton(text="+1", callback_data="num_incr"),
+        ],
+        [types.InlineKeyboardButton(text="Подтвердить", callback_data="num_finish")],
+    ]
+    keyboard = types.InlineKeyboardMarkup(inline_keyboard=buttons)
+    return keyboard
+
+
+async def update_num_text(message: types.Message, new_value: int):
+    with suppress(TelegramBadRequest):
+        await message.edit_text(
+            f"Укажите число: {new_value}",
+            reply_markup=get_keyboard())
+
+
+@dp.message(Command("numbers"))
+async def cmd_numbers(message: types.Message):
+    user_data[message.from_user.id] = 0
+    await message.answer("Укажите число: 0", reply_markup=get_keyboard())
+
+
+@dp.callback_query(F.data.startswith("num_"))
+async def callbacks_num(callback: types.CallbackQuery):
+    user_value = user_data.get(callback.from_user.id, 0)
+    action = callback.data.split("_")[1]
+
+    if action == "incr":
+        user_data[callback.from_user.id] = user_value + 1
+        await update_num_text(callback.message, user_value + 1)
+    elif action == "decr":
+        user_data[callback.from_user.id] = user_value - 1
+        await update_num_text(callback.message, user_value - 1)
+    elif action == "finish":
+        await callback.message.edit_text(f"Итого: {user_value}")
+
+    await callback.answer()
+
+
+@dp.message(Command("random"))
+async def cmd_random(message: types.Message):
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(
+        text="Нажми меня",
+        callback_data="random_value")
+    )
+    await message.answer(
+        "Нажмите на кнопку, чтобы бот отправил число от 1 до 10",
+        reply_markup=builder.as_markup()
+    )
+
+
+@dp.callback_query(F.data == 'random_value')
+async def send_random_value(callback: types.CallbackQuery):
+    await callback.message.answer(str(randint(1, 10)))
+    await callback.answer(text='Спасибо что тыкнул',
+                          show_alert=True)
+
+
+@dp.message(Command("inline_url"))
+async def cmd_inline_url(message: types.Message, bot: Bot):
+    builder = InlineKeyboardBuilder()
+    builder.row(types.InlineKeyboardButton(
+        text="GitHub", url="https://github.com"))
+    builder.row(types.InlineKeyboardButton(
+        text="Оф. канал Telegram", url="tg://resolve?domain=telegram"))
+
+    # Чтобы иметь возможность показать ID-кнопку,
+    # У юзера должен быть False флаг has_private_forwards
+    user_id = message.chat.id
+    chat_info = await bot.get_chat(user_id)
+    if not chat_info.has_private_forwards:
+        builder.row(types.InlineKeyboardButton(text="Какой-то пользователь", url=f"tg://user?id={user_id}"))
+
+    await message.answer(
+        'Выберите ссылку',
+        reply_markup=builder.as_markup(),
+    )
 
 
 @dp.message(Command("special_buttons"))
@@ -52,6 +204,21 @@ async def cmd_special_buttons(message: types.Message):
     await message.answer(
         "Выберите действие:",
         reply_markup=builder.as_markup(resize_keyboard=True),
+    )
+
+@dp.message(F.user_shared)
+async def on_user_shared(message: types.Message):
+    print(
+        f"Request {message.user_shared.request_id}. "
+        f"User ID: {message.user_shared.user_id}"
+    )
+
+
+@dp.message(F.chat_shared)
+async def on_user_shared(message: types.Message):
+    print(
+        f"Request {message.chat_shared.request_id}. "
+        f"User ID: {message.chat_shared.chat_id}"
     )
 
 
@@ -87,7 +254,7 @@ async def with_puree(message: types.Message):
 
 @dp.message(F.text.lower() == 'без пюрешки')
 async def withour_puree(message: types.Message):
-    await message.reply('Так не вкустно!', reply_markup=types.ReplyKeyboardRemove())
+    await message.reply('Так не вкусно!', reply_markup=types.ReplyKeyboardRemove())
 
 
 @dp.message(Command("hidden_link"))
