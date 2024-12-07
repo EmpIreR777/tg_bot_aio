630ccdf7e754919f954538db5f33f0848e02e299
Kirill Safronov <Kirill.tanatos1993@mail.ru>
2024-12-07 22:47:00 +0300

    callback_query

--- handlers/start.py
+++ handlers/start.py
@@ -1,19 +1,43 @@
 from aiogram import Router, F
 from aiogram.filters import CommandStart, Command, CommandObject
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 
 from keyboards.all_kb import create_rat, main_kb, create_spec_kb
-from keyboards.inline_kbs import ease_link_kb
-
+from keyboards.inline_kbs import ease_link_kb, get_inline_kb
+from utils.utils import get_random_person
 
 start_router = Router()
+
+
+@start_router.callback_query(F.data == 'back_home')
+async def cmd_back_home(call: CallbackQuery):
+    await call.answer('Вы вернулись на главный экран!', show_alert=False)
+    await call.message.answer(
+        'Главный экран', reply_markup=main_kb(call.from_user.id)
+    )
+
+
+@start_router.callback_query(F.data == 'get_person')
+async def send_random_person(call: CallbackQuery):
+    await call.answer('Генерирую случайного пользователя', show_alert=False)
+    user = get_random_person()
+    formatted_message = (
+        f"👤 <b>Имя:</b> {user['name']}\n"
+        f"🏠 <b>Адрес:</b> {user['address']}\n"
+        f"📧 <b>Email:</b> {user['email']}\n"
+        f"📞 <b>Телефон:</b> {user['phone_number']}\n"
+        f"🎂 <b>Дата рождения:</b> {user['birth_date']}\n"
+        f"🏢 <b>Компания:</b> {user['company']}\n"
+        f"💼 <b>Должность:</b> {user['job']}\n"
+    )
+    await call.message.answer(formatted_message)
 
 
 @start_router.message(F.text == 'Давай инлайн!')
 async def get_inline_btn_link(message: Message):
     await message.answer(
         'Вот тебе инлай клавиатура со ссылками!',
-        reply_markup=ease_link_kb()
+        reply_markup=get_inline_kb()
     )
 
 
--- keyboards/inline_kbs.py
+++ keyboards/inline_kbs.py
@@ -4,6 +4,16 @@ from aiogram.types import (InlineKeyboardMarkup,
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
+def get_inline_kb():
+    inline_kb_list = [
+        [InlineKeyboardButton(
+            text='Генерировать пользователя', callback_data='get_person')],
+        [InlineKeyboardButton(
+            text='На главную', callback_data='back_home')]
+    ]
+    return InlineKeyboardMarkup(inline_keyboard=inline_kb_list)
+
+
 def ease_link_kb():
     inline_kb_list = [
         [InlineKeyboardButton(
