2551060e51a98c428db741d3aedee8992105ddff
TANATOS <Kirill.tanatos1993@mail.ru>
2024-12-14 17:31:02 +0300

    fix random users-admin

--- handlers/admin_panel.py
+++ handlers/admin_panel.py
@@ -15,19 +15,21 @@ admin_router = Router()
 @admin_router.message(F.text == 'Рандомный пользователь!')
 async def get_random_user(message: Message):
     all_users_data = await get_all_users()
-    user_info = random.choice(all_users_data)
-    profile_message = (
-            f"<b>👤 Профиль пользователя:</b>\n"
-            f"<b>🆔 ID:</b> {user_info['user_id']}\n"
-            f"<b>💼 Логин:</b> @{user_info['user_login']}\n"
-            f"<b>📛 Полное имя:</b> {user_info['full_name']}\n"
-            f"<b>🧑‍🦰 Пол:</b> {user_info['gender']}\n"
-            f"<b>🎂 Возраст:</b> {user_info['age']}\n"
-            f"<b>📅 Дата регистрации:</b> {user_info['date_reg']}\n"
-            f"<b>📝 О себе:</b> {user_info['about']}\n"
-        )
-    await message.answer_photo(photo=user_info.get('photo'), caption=profile_message)
-
+    if all_users_data:
+        user_info = random.choice(all_users_data)
+        profile_message = (
+                f"<b>👤 Профиль пользователя:</b>\n"
+                f"<b>🆔 ID:</b> {user_info['user_id']}\n"
+                f"<b>💼 Логин:</b> @{user_info['user_login']}\n"
+                f"<b>📛 Полное имя:</b> {user_info['full_name']}\n"
+                f"<b>🧑‍🦰 Пол:</b> {user_info['gender']}\n"
+                f"<b>🎂 Возраст:</b> {user_info['age']}\n"
+                f"<b>📅 Дата регистрации:</b> {user_info['date_reg']}\n"
+                f"<b>📝 О себе:</b> {user_info['about']}\n"
+            )
+        await message.answer_photo(photo=user_info.get('photo'), caption=profile_message)
+    else:
+        await message.answer('Пользователей нет!')
 
 @admin_router.message((F.text.endswith('Админ панель')) & (F.from_user.id.in_(admins)))
 async def get_profile(message: Message):
@@ -37,7 +39,7 @@ async def get_profile(message: Message):
         admin_text = f'👥 В базе данных <b>{len(all_users_data)}</b> человек. Вот короткая информация по каждому:\n\n'
 
         for user in all_users_data:
-            admin_text += f'👤 Телеграм ID: {user.get("user_id")}\n' f'📝 Полное имя: {user.get("full_name")}\n'
+            admin_text += f'👤 Телеграм ID: .get("user_id")}\n' f'📝 Полное имя: {user.get("full_name")}\n'
 
             if user.get("user_login") is not None:
                 admin_text += f'🔑 Логин: {user.get("user_login")}\n'
